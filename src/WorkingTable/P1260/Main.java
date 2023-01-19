a1e05e1775e6fa0479d73635edc10cfc1484e501
chang-seop <ikavon@naver.com>
2023-01-19 12:53:11 +0900

    Add DFS와 BFS
    문제 번호 : 1260번
    문제 난이도 : Silver 2

--- src/WorkingTable/P1260/Main.java
+++ src/WorkingTable/P1260/Main.java
@@ -1,92 +1,82 @@
 package WorkingTable.P1260;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<Integer>[] adjList; //인접 리스트
-    static boolean[] visited;
-    static int[] answer;
-    static int idx = 0;
-    static int N, M, V; // N 정점의 개수, M 간선의 개수
+    static boolean[] visitedDfs, visitedBfs;
+    static int N, M, V; // 정점의 개수 N, 간선의 개수 M, 시작할 정점의 번호 V
+    static ArrayList<Integer>[] adjList;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/WorkingTable/P1260/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         V = Integer.parseInt(st.nextToken());
 
-        adjList = new LinkedList[N + 1];
-
-        for (int i = 0; i <= N; i++) {
-            adjList[i] = new LinkedList<>();
+        adjList = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            Integer a = Integer.parseInt(st.nextToken());
+            Integer b = Integer.parseInt(st.nextToken());
 
+            // 입력으로 주어지는 간선은 양방향
             adjList[a].add(b);
             adjList[b].add(a);
         }
-        visited = new boolean[N + 1];
-        answer = new int[N + 1];
 
-        // DFS
-        dfs(V);
-        for(int i = 1; i <= N; i++) sb.append(answer[i]).append(" ");
-        System.out.println(sb);
+        // 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문 하기 위해 오름차순 정렬
+        for (int i = 1; i <= N; i++) Collections.sort(adjList[i]);
+
+        visitedDfs = new boolean[N + 1];
+        visitedBfs = new boolean[N + 1];
+
+        dfs(V, 0);
+        sb.append("\n");
+        bfs();
+
+        // 결과
+
+        System.out.print(sb);
+    }
+
+    private static void bfs() {
+        Queue<Integer> point = new LinkedList<>();
+        point.add(V); // 시작 정점 추가
+        // 시작 정점 체크인
+        visitedBfs[V] = true;
+        sb.append(V).append(" ");
 
         //BFS
-        sb = new StringBuilder();
-        Queue<Integer> queue = new LinkedList<>();
-        visited = new boolean[N + 1];
-        answer = new int[N + 1];
-        idx = 0;
+        while(!point.isEmpty()) {
+            Integer polled = point.poll();
 
-        queue.add(V);
-        visited[V] = true;
-
-        while(!queue.isEmpty()) {
-            //큐에서 가져오기
-            int v = queue.poll();
-            answer[v] = ++idx;
-            //목적지 인가?
-            //연결된 곳 순회
-            for(int i : adjList[v]) {
-
-                //갈 수 있는가?
-                if(!visited[i]) {
-                    //체크인
-                    visited[i] = true;
-                    //큐에 넣기
-                    queue.add(i);
+            for(Integer go : adjList[polled]) {
+                if(!visitedBfs[go]) {
+                    visitedBfs[go] = true;
+                    sb.append(go).append(" ");
+                    point.add(go);
                 }
             }
         }
-
-        for(int i = 1; i <= N; i++) sb.append(answer[i]).append(" ");
-        System.out.print(sb);
     }
-    static void dfs(int index)  {
-        // 체크인
-        visited[index] = true;
-        answer[index] = ++idx;
-        // 목적지 인가?
-        // 연결된 곳 순회하기
-        for(int i : adjList[index]) {
-            // 갈 수 있는가?
-            if(!visited[i]) {
-                // 간다
-                dfs(i);
+
+    private static void dfs(int index, int count) {
+        visitedDfs[index] = true;
+        sb.append(index).append(" ");
+
+        for(Integer go : adjList[index]) {
+            if(!visitedDfs[go]) {
+                dfs(go, count + 1);
             }
         }
-        // 체크아웃
     }
+
 }
